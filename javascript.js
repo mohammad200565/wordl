202fdd9c82f0d399e705aa84fd48b78636f0abbb
Mohammad <mohammadalhomsi4444@gmail.com>
2024-03-07 16:34:29 +0300

    add play again button

--- javascript.js
+++ javascript.js
@@ -1,8 +1,9 @@
 /*      start Generating The word           */
 
-        let the_dictionary = Array(
-            "Judge" , "Mouse" , "alien" , "knife" , "night" , "write" , "eager" , "eagle" , "young" , "youth"
-        );
+        let the_dictionary = [
+            "Judge" , "Mouse" , "alien" , "knife" , "night" , "write" , "eager" , "eagle" , 
+            "young" , "youth" , "royal" , "house" , "horse" , "legal" , "wreck" , "beach"
+        ];
         let Ran = Math.floor( Math.random() *the_dictionary.length);
         let the_word = the_dictionary[Ran];
 /*      end Generating The word         */
@@ -12,7 +13,50 @@ let The_number_of_the_word = 1;
 let The_number_of_the_letter = 1;
 
 document.addEventListener("click" , (e)=>{
-    console.log(window.innerWidth);
+
+    if(e.target.className === "playAgain") {
+        The_number_of_the_word = 1;
+        The_number_of_the_letter = 1;
+        document.querySelector(".words").innerHTML = `  <div class="attempt">
+                                                            <span class="oneLetter"></span>
+                                                            <span class="oneLetter"></span>
+                                                            <span class="oneLetter"></span>
+                                                            <span class="oneLetter"></span>
+                                                            <span class="oneLetter"></span>
+                                                        </div>
+                                                        <div class="attempt">
+                                                            <span class="oneLetter"></span>
+                                                            <span class="oneLetter"></span>
+                                                            <span class="oneLetter"></span>
+                                                            <span class="oneLetter"></span>
+                                                            <span class="oneLetter"></span>
+                                                        </div>
+                                                        <div class="attempt">
+                                                            <span class="oneLetter"></span>
+                                                            <span class="oneLetter"></span>
+                                                            <span class="oneLetter"></span>
+                                                            <span class="oneLetter"></span>
+                                                            <span class="oneLetter"></span>
+                                                        </div>
+                                                        <div class="attempt">
+                                                            <span class="oneLetter"></span>
+                                                            <span class="oneLetter"></span>
+                                                            <span class="oneLetter"></span>
+                                                            <span class="oneLetter"></span>
+                                                            <span class="oneLetter"></span>
+                                                        </div>
+                                                        <div class="attempt">
+                                                            <span class="oneLetter"></span>
+                                                            <span class="oneLetter"></span>
+                                                            <span class="oneLetter"></span>
+                                                            <span class="oneLetter"></span>
+                                                            <span class="oneLetter"></span>
+                                                        </div>`;
+        document.querySelector(".theScore").style.display = "none";
+        document.querySelector(".container").style.opacity = 1;
+        Ran = Math.floor( Math.random() *the_dictionary.length);
+        the_word = the_dictionary[Ran];
+    }
 
     if(e.target.className === "delete" && The_number_of_the_letter>1 ) {
         let the_square = document.querySelector(".words :nth-child(" + The_number_of_the_word + ") :nth-child(" + (The_number_of_the_letter-1) + ")");
@@ -55,14 +99,14 @@ document.addEventListener("click" , (e)=>{
 
         if(result || The_number_of_the_word==5) {
             let score = document.querySelector(".theScore")
-            if(result)   score.innerHTML  = 'Congratulations!' ;
+            if(result)   score.innerHTML  = '<img src="images\\Congratulations.png" class="gameOver">' ;
             else        score.innerHTML = '<img src="images\\GameOver.png" class="gameOver">';
-            score.innerHTML += '<div class="theWord">The word is ' + the_word.toLocaleUpperCase() +'</div>';
+            score.innerHTML += '<div class="theWord">The word is ' + the_word.toLocaleUpperCase() +'</div><div class="playAgain">play again</div>';
             score.style.display = "block";
             document.querySelector(".container").style.opacity = "0.6" ;
-            document.querySelector("*").style.cursor = "auto" ;
         }
         The_number_of_the_word++;
         The_number_of_the_letter = 1;
     }
 });
+
--- letters.js
+++ letters.js
@@ -1,70 +1,27 @@
 /*      start Generating the Letters        */
         console.log(window.innerWidth);
-        let LL = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-        let alpha = Array.from(LL);
-        let container = document.querySelector(".container");
-        let letters = document.createElement("div");
+        let LL = [ "QWERTYUIOP" , "ASDFGHJKL" , "ZXCVBNM" ];
 
-        let p1 = document.querySelector(".p1");
-        let p2 = document.querySelector(".p2");
+        let letters = '<div class="keyboard">';
 
-        p1.appendChild(letters);
-
-        letters.innerHTML = '<span class="submit">Submit</span><span class="delete">Delete</span>';
-        letters.className = "letters";
-
-        alpha.forEach((x)=>{
-            let span = document.createElement("span");
-            let l = document.createTextNode(x);
-            span.appendChild(l);
-            letters.appendChild(span);
-            span.innerHTML = x;
-            span.className = "letterButtom"
-        });
-
-
-        letters = document.createElement("div");
-
-        p2.appendChild(letters);
-
-        letters.innerHTML = '<span class="submit">Submit</span><span class="delete">Delete</span>';
-        letters.className = "letters";
-
-        alpha.forEach((x)=>{
-            let span = document.createElement("span");
-            let l = document.createTextNode(x);
-            span.appendChild(l);
-            letters.appendChild(span);
-            span.innerHTML = x;
-            span.className = "letterButtom"
-        });
-
-        if( window.innerWidth>= 992) {
-            p1.style.display = "none";
-            p2.style.display = "block";
+        for(let i=0;i<3;i++) {
+            letters += '<div class="letters">';
+            let alpha = Array.from(LL[i]);
+            alpha.forEach((x)=>{
+                letters += '<span class="letterButtom">' + x + '</span>';
+            });
+            if(i===1) letters += '<span class="delete">Delete</span>';
+            if(i===2) letters += '<span class="submit">Submit</span>';
+            letters += '</div>';
+            
         }
-        else {
-            p2.style.display = "none";
-            p1.style.display = "block";        
-        }
+
+        letters += '</div>';
+
+        document.querySelector(".p1").innerHTML = letters;
+        document.querySelector(".p2").innerHTML = letters;
+
+        //_____________________________________
 
 /*      end Generating the Letters          */
-
-let W=window.innerWidth;
-function f() {
-    if(window.innerWidth!=W) {
-        if( window.innerWidth>= 992) {
-            p1.style.display = "none";
-            p2.style.display = "block";
-        }
-        else {
-            p2.style.display = "none";
-            p1.style.display = "block";        
-        }
-        W = window.innerWidth;
-    }
-}
-
-setInterval( f , 50);
-
 
